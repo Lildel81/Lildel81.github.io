b12760725989a542f9835fea5cd89dc22fb3490c
Lildel81 <103783285+Lildel81@users.noreply.github.com>
2024-02-18 17:34:01 -0800

    First Commit
    A site that will have several Math equations available on it.

---
+++ quadratic.js
@@ -0,0 +1,28 @@
+function displayText(){
+    // Get the value from the text box
+    let A = document.getElementById("inputTextA").value;
+    let B = document.getElementById("inputTextB").value;
+    let C = document.getElementById("inputTextC").value;
+    // Display the value on the webpage
+
+    // Building the value of the quotient;
+    
+    let r = Math.sqrt((B*B)-(4*A*C))
+    // Building the Denominator
+    let d = 2*A;
+    //Building the Formula
+    let f = ((0-B)-r)/d;
+    let F = ((0-B)+r)/d;
+
+    document.getElementById("output").innerText = "The result is " + f + " and " + F; 
+
+    /*let first = (0-B);
+    let rad = Math.sqrt(Math.pow(b, 2)-4 * A * C);
+    let denominator = (2 * A);
+
+    let root1 = (first - rad)/denominator;
+    let root2 = (first + rad)/denominator;
+    
+    document.getElementById("R1").innerText = "The first root is " + root1;
+    document.getElementById("R2").innerText = "The second root is " + root2;*/
+}
